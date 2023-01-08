fe546a035ceeaf0748f967d2a96baab4543b2055
Misida Krakulli <misidakrakulli@gmail.com>
2023-01-08 19:52:52 +0100

    Promises

---
+++ esercizio-11/luckyDraw.mjs
@@ -0,0 +1,23 @@
+const players = ["Joe", "Caroline", "Sabrina"];
+
+
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+
+  players.forEach(person => {
+    luckyDraw(person)
+    .then(result => console.log(result))
+    .catch(error => console.error(error.message));
+});
