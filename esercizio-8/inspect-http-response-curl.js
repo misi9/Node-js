517e4fb178a4e1bca25c5755c6cbf13eb0147744
Misida Krakulli <misidakrakulli@gmail.com>
2023-01-08 15:50:23 +0100

    Inspect an http response with curl

---
+++ esercizio-8/inspect-http-response-curl.js
@@ -0,0 +1,4 @@
+// Inspect an HTTP response with curl
+// Make an HTTP request with curl that shows the response headers for this URL: https://jsonplaceholder.typicode.com/posts/1/comments
+// What is the value of the content-type response header?
+//content-type: application/json; charset=utf-8
