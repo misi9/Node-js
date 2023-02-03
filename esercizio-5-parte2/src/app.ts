77d6c58e64d3166db514e156c661497bc120613f
Misida Krakulli <misidakrakulli@gmail.com>
2023-02-03 22:15:31 +0100

    handle file uploads

---
+++ esercizio-5-parte2/src/app.ts
@@ -0,0 +1,88 @@
+import express from "express";
+import {PrismaClient} from "@prisma/client";
+import cors from "cors";
+import {initMulterMiddleware} from "./lib/middleware/multer";
+
+import {
+    validate,
+    planetSchema,
+    PlanetData,
+    validationErrorMiddleware,
+
+} from "./lib/validation"
+
+const corsOption = {
+    origin: "http://localhost:8080"
+};
+const upload = initMulterMiddleware();
+
+const app = express();
+const prisma = new PrismaClient();
+app.use(cors(corsOption));
+
+app.get('/planets', async(request, response)=>{
+   const planets = await prisma.planet.findMany()
+   response.json(planets) 
+})
+
+app.get('/planets/:id', async(request, response)=>{
+    const id = request.params
+    const planet = await prisma.planet.findUnique({
+        where: {
+            id: +id 
+        }
+    })
+    response.json(planet)
+ })
+
+app.post('/planets',validate({body: planetSchema}), async (request, response) => {
+    const planetData :  PlanetData = request.body
+    const planet = await prisma.planet.create({
+        data: planetData
+    })
+    response.json(planet)
+    
+})
+
+app.put('/planets/:id', async(request, response)=>{
+    const id = request.params
+    const {name,diameter,moons} = request.body
+    const planet = await prisma.planet.update({
+        where: {
+          id: +id 
+        },
+        data:{
+          name,
+          diameter,
+          moons
+        }
+    })
+    response.json(planet)
+})
+
+app.delete('/planets/:id', async(request, response)=>{
+    const id = request.params
+    const planet = await prisma.planet.delete({
+        where: {
+          id: +id 
+        }
+    })
+    response.json(planet)
+})
+
+app.post("/planets/:id/photo", 
+upload.single("photo"), 
+async (request, response) => {
+    console.log("request.file", request.file)
+
+    const photoFilename = request.file?.filename
+    response.json({photoFilename})
+})
+
+app.use(validationErrorMiddleware);
+
+
+app.listen(3000, ()=>{
+    console.log('running at port 3000')
+})
+
